5757370a71c958d1af8b8503459476b35300b38f
Bonyeong1998 <bonyoung1998@gmail.com>
2022-06-04 15:20:30 +0900

    파일 업로드

--- src/main/java/io/github/dorma/webrtc/Application.java
+++ src/main/java/io/github/dorma/webrtc/Application.java
@@ -1,11 +1,16 @@
 package io.github.dorma.webrtc;
 
+import io.github.dorma.webrtc.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableWebSocket
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class Application {
 
 	static {
