4f091bd50224311b724f2185c74cbb5cb2587fbb
Bonyeong1998 <bonyoung1998@gmail.com>
2022-06-08 09:53:51 +0900

    WebSocket Connect Error fix

--- src/main/java/io/github/dorma/webrtc/controller/MainController.java
+++ src/main/java/io/github/dorma/webrtc/controller/MainController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
